a62e9cfef8fd5f968d9699b463cb4370fbd18f1d
Cesar Rodriguez <4625096+cesar-rodriguez@users.noreply.github.com>
2023-09-28 10:34:35 -0400

    converting to fastapi for now

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 import os
-
-from flask import Flask, jsonify, abort, send_from_directory
-from flask_swagger_ui import get_swaggerui_blueprint
 import boto3
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.models import APIKey
+from fastapi.openapi.models import APIKeyIn
+from fastapi import Depends, HTTPException, status
+from pydantic import BaseModel
 
-
-app = Flask(__name__)
-
-# Setup OpenAPI
-SWAGGER_URL = '/docs'
-API_URL = '/docs/openapi.yaml'
-swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Presign API"
-    }
-)
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-
+app = FastAPI()
 
 # Setup boto3 client
 s3_client = boto3.client("s3")
@@ -26,8 +15,12 @@ s3_client = boto3.client("s3")
 S3_BUCKET = os.getenv("S3_BUCKET")  # Load the S3 bucket from environment variable
 
 
-@app.route("/presign/<s3_key>", methods=["GET"])
-def get_presigned_url(s3_key):
+class PreSignedUrlResponse(BaseModel):
+    presigned_url: str
+
+
+@app.get("/presign/{s3_key}", response_model=PreSignedUrlResponse)
+async def get_presigned_url(s3_key: str):
     try:
         # Generate a pre-signed URL for the object
         presigned_url = s3_client.generate_presigned_url(
@@ -36,18 +29,20 @@ def get_presigned_url(s3_key):
             ExpiresIn=3600,  # The URL will be valid for 1 hour by default
         )
     except Exception as e:
-        abort(500, description="Error generating pre-signed URL")
+        raise HTTPException(status_code=500, detail="Error generating pre-signed URL")
 
     if not presigned_url:
-        abort(404, description="Failed to generate the presigned URL")
+        raise HTTPException(
+            status_code=404, detail="Failed to generate the presigned URL"
+        )
 
-    return jsonify({"presigned_url": presigned_url})
-
-
-@app.route('/docs/openapi.yaml')
-def serve_openapi():
-    return send_from_directory('docs', 'openapi.yaml')
+    return {"presigned_url": presigned_url}
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
+
+
+# serve OpenAPI yaml file
+app.mount("/docs", StaticFiles(directory="docs", html=True), name="docs")
